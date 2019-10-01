23b71090aba9eb0b1ede1bd87127a7dd0e32c9ef
Publicidad-abierta <publicidadabierta@inai.org.mx>
2019-10-01 10:59:41 -0500

    Publicidad_abierta

---
+++ tpov1/application/views/tpov1/includes/footer.php
@@ -0,0 +1,136 @@
+<?php
+
+?>
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.content-wrapper -->
+        
+        <footer class="footer">
+            <div class="container">
+                <div class="col-md-12 espacio">
+                    <div class="col-md-4">
+                        <a href="http://inicio.inai.org.mx" target="_blank">
+                            <img class="img-footer" src="<?php echo base_url() . 'dist/img/inai-footer-04.png' ?>"> 
+                        </a>
+
+                        <a class="ainstitucion" href="http://inicio.inai.org.mx" target="_blank">inai.org.mx</a>
+                        <div class="text-footer">
+                            <p>Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales</p>
+                            <p>Insurgentes Sur No. 3211 Col. Insurgentes Cuicuilco. Delegación Coyoacán, C.P. 04530</p>
+                            <p>Telinai: 01800 8354 324</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="http://fundar.org.mx" target="_blank">
+                            <img class="img-footer" src="<?php echo base_url() . 'dist/img/fundar-footer-04.png' ?>"> 
+                        </a>
+
+                        <a class="ainstitucion" href="http://fundar.org.mx" target="_blank">fundar.org.mx</a>
+                        <div class="text-footer">
+                            <p>Fundar, Centro de Análisis e Investigación A.C.</p>
+                            <p>Cerrada de Alberto Zamora 21, Col. Villa Coyoacán. Delegación Coyoacán, C.P. 04000</p>
+                            <p>Tel: +52 (55) 5554 3001</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class="h3institucion" >Social</h3>
+                        <a href="https://www.facebook.com/INAImx/" target="_blank">
+                            <img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/fb-bco.png' ?>" />
+                        </a>
+                        <a href="https://twitter.com/INAImexico" target="_blank">
+                            <img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/tw-bco.png' ?>" />
+                        </a>
+                        <a href="https://github.com/INAImexico" target="_blank">
+                            <img class="a-redes-footer" src="<?php echo base_url() . 'dist/img/gh-bco.png' ?>" />
+                        </a>
+                        <h3 class="h3interes" >Ligas de inter&eacute;s</h3>
+                        <div class="brick">
+                            <ul>
+                                <li>
+                                    <a href="https://www.plataformadetransparencia.org.mx/web/guest/sac" target="_blank">
+                                        Sistema de Solicitudes de Acceso a la Información
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.plataformadetransparencia.org.mx/web/guest/mi" target="_blank">
+                                        Sistema de Gestión de Medios de Impugnación
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.plataformadetransparencia.org.mx/web/guest/sistema-portales" target="_blank">
+                                        Sistema de Portales de Obligaciones de Transparencia
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 espacio">
+                    <div class="pleca-footer"></div>
+                    <div class="bottomFooter">
+                        Transparencia en publicidad oficial - TPO Ver. 2.0. Es una plataforma de código abierto basada en el prototipo funcional desarrollado por el Gobierno del Estado de Oaxaca 2011 – 2016 y Fundar.
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+    
+    <!-- Bootstrap 3.3.2 JS -->
+    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap-slider.js" type="text/javascript"></script>
+    <!-- DATA TABES SCRIPT -->
+    <script src="<?php echo base_url(); ?>plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+    <!-- SlimScroll -->
+    <script src="<?php echo base_url(); ?>plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <!-- FastClick -->
+    <script src='<?php echo base_url(); ?>plugins/fastclick/fastclick.min.js'></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo base_url(); ?>dist/js/app.min.js" type="text/javascript"></script>
+    <!-- fullCalendar 2.2.5 -->
+    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.7.0/moment.min.js" type="text/javascript"></script>-->
+    <script src="<?php echo base_url(); ?>plugins/js/moment-2.7.0.min.js" type="text/javascript"></script>
+    <script src="<?php echo base_url(); ?>plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+    <!-- Page specific script -->
+    <!-- cK Editor -->
+    <script src="<?php echo base_url(); ?>plugins/ckeditor/ckeditor.js" type="text/javascript"></script>
+    <!-- datepicker -->
+    <script src="<?php echo base_url(); ?>plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>plugins/datepicker/datepicker3.css">
+    <!-- bootbox -->
+    <script src="<?php echo base_url();?>plugins/bootboxjs/bootbox.min.js"></script>
+    <!-- charts-->
+    <script src="<?php echo base_url(); ?>plugins/highcharts/highstock.js"></script>
+    <!--<script src="<?php echo base_url(); ?>plugins/highcharts/highcharts.js"></script>-->
+    <script src="<?php echo base_url(); ?>plugins/highcharts/sankey.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/highcharts-more.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/networkgraph.js"></script> 
+    <script src="<?php echo base_url(); ?>plugins/highcharts/data.js">></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/drilldown.js"></script>
+    <script src="<?php echo base_url(); ?>plugins/highcharts/map.js"></script>
+
+    
+    <script>
+    $(function(){
+        $(window).on("scroll",function(){
+            if( $(window).scrollTop()>$(".navbar").height() && $(".navbar").is(".navbar-static-top")){
+                var e=$(".navbar").height();
+                $(".navbar").css("top",-1*e).removeClass("navbar-static-top").addClass("navbar-fixed-top").animate({top:0})
+            }
+
+            if($(window).scrollTop()<$(".navbar").height() && $(".navbar").is(".navbar-fixed-top")){
+                var e=$(".navbar").height();
+                $(".navbar").removeClass("navbar-fixed-top").addClass("navbar-static-top").animate();
+            }
+        });
+    });
+    </script>
+    <?php echo $scripts; ?>
+    
+</body>
+</html>
+        
---
+++ tpov1/scripts/inai/Alter_table_tab_presupuestos.sql
@@ -0,0 +1,14 @@
+
+--
+-- Modificaciones en la tabla `tab_presupuestos`
+--
+
+ALTER TABLE tab_presupuestos ADD COLUMN `mision` text  AFTER `nota`;
+ALTER TABLE tab_presupuestos ADD COLUMN `objetivo` text  AFTER `mision`;
+ALTER TABLE tab_presupuestos ADD COLUMN `metas` text  AFTER `objetivo`;
+ALTER TABLE tab_presupuestos ADD COLUMN `programas` text  AFTER `metas`;
+ALTER TABLE tab_presupuestos ADD COLUMN `objetivo_transversal` text  AFTER `programas`;
+ALTER TABLE tab_presupuestos ADD COLUMN `conjunto_campanas` text  AFTER `objetivo_transversal`;
+ALTER TABLE tab_presupuestos ADD COLUMN `temas` text  AFTER `conjunto_campanas`;
+ALTER TABLE tab_presupuestos ADD COLUMN `nota_planeacion` text  AFTER `temas`;
+
